1bc83dad5b420dee401b1ef67ca7f982b596c151
Daniel Paz <danielpaz601@gmail.com>
2020-04-15 22:22:36 +0300

    Updates

---
+++ precache-manifest.294ecf19f5deb4855e6b70ceb57c11ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cde101fe37e1c7d569a7efc0082f4858",
+    "url": "/React-Neural-Network-Planner/index.html"
+  },
+  {
+    "revision": "13f8eba0ddf5f55ceb2b",
+    "url": "/React-Neural-Network-Planner/static/css/main.c7c76d55.chunk.css"
+  },
+  {
+    "revision": "70e2b8046e8a0565c70e",
+    "url": "/React-Neural-Network-Planner/static/js/2.056b61b4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/React-Neural-Network-Planner/static/js/2.056b61b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13f8eba0ddf5f55ceb2b",
+    "url": "/React-Neural-Network-Planner/static/js/main.0f24c1b2.chunk.js"
+  },
+  {
+    "revision": "ef7b60b49f1303a74227",
+    "url": "/React-Neural-Network-Planner/static/js/runtime-main.05cd5a7a.js"
+  }
+]);
