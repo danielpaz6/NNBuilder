e1b24e3a60d48ecc40771b3512faf91d707af1ff
Daniel Paz <danielpaz601@gmail.com>
2023-07-01 00:09:33 +0300

    Fixed urls

--- precache-manifest.ecd6138c1cf3fcc6deaeb49333f8a125.js
+++ precache-manifest.ecd6138c1cf3fcc6deaeb49333f8a125.js
@@ -5,26 +5,26 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   },
   {
     "revision": "dbc67cf9d48a00c577df",
-    "url": "/static/css/main.1478061a.chunk.css"
+    "url": "/NNBuilder/static/css/main.1478061a.chunk.css"
   },
   {
     "revision": "415b7ac3bf297f6e2240",
-    "url": "/static/js/2.d79790e5.chunk.js"
+    "url": "/NNBuilder/static/js/2.d79790e5.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/static/js/2.d79790e5.chunk.js.LICENSE.txt"
+    "url": "/NNBuilder/static/js/2.d79790e5.chunk.js.LICENSE.txt"
   },
   {
     "revision": "dbc67cf9d48a00c577df",
-    "url": "/static/js/main.aad930cc.chunk.js"
+    "url": "/NNBuilder/static/js/main.aad930cc.chunk.js"
   },
   {
     "revision": "85415efe929349ff9b6f",
-    "url": "/static/js/runtime-main.fc4538ee.js"
+    "url": "/NNBuilder/static/js/runtime-main.fc4538ee.js"
   },
   {
     "revision": "edf3373fab1c55917650e88e56c1e439",
-    "url": "/static/media/logo.edf3373f.svg"
+    "url": "/NNBuilder/static/media/logo.edf3373f.svg"
   }
 ]);
